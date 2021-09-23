538104aaef51c36448accb93459fe87292e75207
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-22 18:58:25 -0700

    Routed the app page with homepage and aboutpage so far.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { HomePage, AboutPage } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <AboutPage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
