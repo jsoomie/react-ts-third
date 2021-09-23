2531d09951941e125e3782337b21f45dc6502697
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-23 16:59:24 -0700

    Added basic ts types and stylings for the review page. Starting on object review.

--- src/components/TSReview/TSReview.tsx
+++ src/components/TSReview/TSReview.tsx
@@ -1,9 +1,56 @@
+import "./TSReview.css";
+
 export const TSReview = () => {
-  return (
-    <div>
+  // Core Types: number, string, boolean ////
+  const Basic = () => {
+    const firstNumber = 5;
+    const secondNumber = 2.3;
+    const result = true;
+    const add = (one: number, two: number, show: boolean) => {
+      if (show) {
+        return one + two;
+      } else {
+        return "Nothing to show...";
+      }
+    };
+
+    return (
+      <div id="basic-ts" className="review-card">
+        <h2>Number, String, Boolean</h2>
+        <p>{add(firstNumber, secondNumber, result)}</p>
+      </div>
+    );
+  };
+  // Core Types: number, string, boolean //
+
+  // Core Types: Object ////
+  const ObjectTS = () => {
+    const person = {
+      name: "Kena",
+      age: 20,
+    };
+
+    return (
+      <div id="object-ts" className="review-card">
+        <h2>Objects</h2>
+        <ul>
+          <li>{person.name}</li>
+          <li>{person.age}</li>
+        </ul>
+      </div>
+    );
+  };
+  // Core Types: Object //
+
+  // STAGE ////
+  const Stage = () => (
+    <div id="reviewContainer">
       <h1>TS review page</h1>
+      <Basic />
+      <ObjectTS />
     </div>
   );
+  return <Stage />;
 };
 
 export default TSReview;
